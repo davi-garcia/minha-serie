a410b4ca8ad00f38dff8339fed40e85bb56712c9
Davi Garcia <davi100msn@gmail.com>
2025-04-09 11:04:36 -0300

    teste erro

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,21 @@
-self.addEventListener('install', (event) => {
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('v1').then((cache) => {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/style.css', // Adicione seus arquivos CSS
-        '/script.js', // Adicione seus arquivos JS
-        'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css',
-        // Adicione outros arquivos que você deseja armazenar em cache
-      ]);
-    })
+      caches.open('my-cache').then(cache => {
+          return cache.addAll([
+              '/index.html',
+              '/styles.css',
+              '/script.js'
+          ]);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+      caches.match(event.request)
+          .then(response => {
+              return response || fetch(event.request);
+          })
   );
 });
 
@@ -20,3 +26,15 @@ self.addEventListener('fetch', (event) => {
     })
   );
 });
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+      navigator.serviceWorker.register('/service-worker.js')
+          .then(registration => {
+              console.log('Service Worker registrado com sucesso:', registration);
+          })
+          .catch(error => {
+              console.error('Registro de Service Worker falhou:', error);
+          });
+  });
+}
